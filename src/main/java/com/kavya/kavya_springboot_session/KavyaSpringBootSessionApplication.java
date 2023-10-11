fc7b070ee5aa7734a39fe033d74db861d526829d
codetalks95 <info@codetalksdna.com>
2023-10-11 08:42:47 +0530

    New SpringBoot Project

---
+++ src/main/java/com/kavya/kavya_springboot_session/KavyaSpringBootSessionApplication.java
@@ -0,0 +1,12 @@
+package com.kavya.kavya_springboot_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavyaSpringBootSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KavyaSpringBootSessionApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kavya/kavya_springboot_session/KavyaSpringBootSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavya.kavya_springboot_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavyaSpringBootSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
