8eb6374f30c2e6fc0b47da1e8e5617312afaeead
codetalks95 <info@codetalksdna.com>
2023-10-16 09:23:15 +0530

    Added the new Code.

--- src/main/java/com/kavya/kavya_springboot_session/EmployeeController.java
+++ src/main/java/com/kavya/kavya_springboot_session/EmployeeController.java
@@ -1,26 +1,42 @@
 package com.kavya.kavya_springboot_session;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class EmployeeController {
 
-    @Autowired
     EmployeeService employeeService;
 
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @PostMapping("/saveEmployee")
-    public Employee saveEmployee(@RequestBody Employee employee){
+    public Employee saveEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
 
     @GetMapping("/getEmployee")
-    public List<Employee> saveEmployee(){
-        return employeeService.saveEmployee();
+    public List<Employee> getAllEmployee() {
+        return employeeService.getAllEmployee();
+    }
+
+    @DeleteMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable String id) {
+        return employeeService.deleteEmployee(id);
+    }
+
+    @PutMapping("/updateEmployee")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.updateEmployee(employee);
+    }
+
+    @PatchMapping("/updatePartialEmployee")
+    public Employee updatePartialEmployee(@RequestBody Employee employee) {
+        return employeeService.updatePartialEmployee(employee);
     }
 }
--- src/main/java/com/kavya/kavya_springboot_session/EmployeeService.java
+++ src/main/java/com/kavya/kavya_springboot_session/EmployeeService.java
@@ -4,18 +4,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
     //IOC :- INVERSION OF CONTROL
-    @Autowired
+
     EmployeeRepository employeeRepository;
 
-    public Employee saveEmployee(Employee employee){
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
-    public List<Employee> saveEmployee(){
+    public List<Employee> getAllEmployee() {
         return employeeRepository.findAll();
+    }
+
+    public String deleteEmployee(String id) {
+        if (employeeRepository.findById(id).isPresent()) {
+            employeeRepository.deleteById(id);
+            return "Successfully Deleted";
+        } else {
+            return "The Id doesn't exist";
+        }
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        if (employeeRepository.findById(employee.getEmployeeId()).isPresent()) {
+            return employeeRepository.save(employee);
+        } else {
+            return new Employee();
+        }
+    }
+
+    public Employee updatePartialEmployee(Employee employee) {
+        Optional<Employee> employee2 = employeeRepository.findById(employee.getEmployeeId());
+        if (employee2.isPresent()) {
+            Employee employee1 = employee2.get();
+            if (employee.getSalary() == 0) {
+                employee1.setSalary(employee1.getSalary());
+            } else {
+                employee1.setSalary(employee.getSalary());
+            }
+            if (employee.getCompanyNames() == null) {
+                employee1.setCompanyNames(employee1.getCompanyNames());
+            } else {
+                employee1.setCompanyNames(employee.getCompanyNames());
+            }
+            if (employee.getCompanyName() == null) {
+                employee1.setCompanyName(employee1.getCompanyName());
+            } else {
+                employee1.setCompanyName(employee.getCompanyName());
+            }
+            return employeeRepository.save(employee1);
+        }
+        return employee;
     }
 }
