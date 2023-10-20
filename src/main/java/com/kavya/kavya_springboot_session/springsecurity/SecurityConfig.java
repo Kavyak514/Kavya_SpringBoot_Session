cc936b57e954a11f397c609f5aa4b2c9bef89507
codetalks95 <info@codetalksdna.com>
2023-10-20 12:18:58 +0530

    Fixed Security Config Issue.

--- src/main/java/com/kavya/kavya_springboot_session/springsecurity/SecurityConfig.java
+++ src/main/java/com/kavya/kavya_springboot_session/springsecurity/SecurityConfig.java
@@ -1,4 +1,50 @@
 package com.kavya.kavya_springboot_session.springsecurity;
 
-public class SecurityConfig extends Websecuritycon{
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("nitesh")
+                .password(passwordEncoder().encode("bhushan123"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("kumar")
+                .password(passwordEncoder().encode("kumar123"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/saveCredentials", "/getEmployee").hasRole("ADMIN")
+                                .requestMatchers("/saveEmployee/**")
+                                .authenticated())
+                .httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
